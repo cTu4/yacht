a3c87eba301e4bd8d24242fc4b39d3138f2f875d
cTu4 <chudik9898@mail.ru>
2020-12-20 19:52:37 +0300

    fix product update page

--- app/addons/my_module/controllers/backend/products.post.php
+++ app/addons/my_module/controllers/backend/products.post.php
@@ -25,6 +25,6 @@ $reservations = db_get_array("select * from ?:product_reservations where product
 if($reservations){
     Tygh::$app['view']->assign('reservations', $reservations);
 }
-    Tygh::$app['view']->assign('lang', $_REQUEST['sl']?$_REQUEST['sl']:'ru');
+    Tygh::$app['view']->assign('lang', isset($_REQUEST['sl'])?$_REQUEST['sl']:'ru');
 
 }
