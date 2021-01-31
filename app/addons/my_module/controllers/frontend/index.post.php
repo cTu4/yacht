485d5f5ca00df3bd8cf36dde101b0d7f11c94bfb
cTu4 <chudik9898@mail.ru>
2021-01-31 18:00:46 +0300

    фикс  id variant for search

--- app/addons/my_module/controllers/frontend/index.post.php
+++ app/addons/my_module/controllers/frontend/index.post.php
@@ -11,18 +11,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     return;
 }
 if ($mode == 'index'){
+    $id_variant_lux = db_get_field("select variant_id from ?:product_feature_variant_descriptions where variant='Люкс'");
     $params = [
         "feature_variants" => [
-            "26" => ["26" => 59]
+            "26" => ["26" => $id_variant_lux]
         ]
     ];
     list($lux_yachts, $search_lux) = fn_get_products($params, Registry::get('settings.Appearance.products_per_page'));
 
 //    fn_filters_handle_search_result($params, $lux_yachts, $search_lux);
+    $id_variant_checked = db_get_field("select variant_id from ?:product_feature_variant_descriptions where variant='Проверено'");
 
     $params = [
         "feature_variants" => [
-            "26" => ["26" => 57]
+            "26" => ["26" => $id_variant_checked]
         ]
     ];
     list($checked_yachts, $search_checked) = fn_get_products($params, Registry::get('settings.Appearance.products_per_page'));
@@ -34,7 +36,6 @@ if ($mode == 'index'){
 //        'get_features'=> true
 //    ));
     $feature_ids = fn_get_features_ids(features_names);
-
     $result = [];
     foreach ($lux_yachts as $yacht){
         $result[] = fn_get_product_data($yacht['product_id'],$auth);
